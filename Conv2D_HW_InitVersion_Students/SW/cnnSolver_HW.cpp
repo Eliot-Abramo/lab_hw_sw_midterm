4d9711c2ff6d476b240d22ca151ddd3d3de7c733
Eliot Abramo <eliot.abramo@epfl.ch>
2025-03-20 13:11:18 +0000

    working but wrong probabilities, cat when dog sometimes.

--- Conv2D_HW_InitVersion_Students/SW/cnnSolver_HW.cpp
+++ Conv2D_HW_InitVersion_Students/SW/cnnSolver_HW.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50a1ab6c24409b0f08c04c3ae0b4b14183ab82c0cf5dd1ae058efbf019ebd725
-size 5878
+oid sha256:8c888e0d65cdc43237ffdf6682698eedccfbdc1f77a0f27286958d7fc9de93fa
+size 5899
--- Conv2D_HW_InitVersion_Students/SW/runAll.sh
+++ Conv2D_HW_InitVersion_Students/SW/runAll.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c515ffc1d79eda7a836cf12a01692bf340d515de75a8d7d871c3dc306aa12d9b
-size 477
+oid sha256:5d1bf657eea937279daeb8499a47ae57da7c4e0bd03bf9326a9c6c7418e42b80
+size 482
