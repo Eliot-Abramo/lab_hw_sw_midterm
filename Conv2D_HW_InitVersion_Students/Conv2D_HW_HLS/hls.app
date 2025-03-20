7eba75edeb48df08cacd38ab142bca836c718f94
Eliot Abramo <eliot.abramo@epfl.ch>
2025-03-20 13:12:00 +0000

    remove HLS files

--- Conv2D_HW_InitVersion_Students/Conv2D_HW_HLS/hls.app
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ca719d2a081953770800a47e6b559d5dfe8d64fc5d0ea1f02e1facd422e27bf8
-size 692
--- Conv2D_HW_InitVersion_Students/Conv2D_HW_HLS/solution1/script.tcl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9de40497daed320457b71942185909955bd3e50830438830153345af3b0bc799
-size 909
