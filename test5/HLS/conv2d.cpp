2ffa2b9d9a547e20c4a4efaebcd3dc5350d8ac9c
Eliot Abramo <eliot.abramo@epfl.ch>
2025-03-25 00:16:58 +0100

    working test 5 but much slower

--- test5/HLS/conv2d.cpp
+++ test5/HLS/conv2d.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:909e072a6efd6a8be1b69b80a7deb72a1915cf5119c8d1c5c323fb4dca2eb4d5
+oid sha256:3c88b422b805af7c649d6f04c69a2048bd958feaca929b9fbdde0a9b0a0bd532
 size 3117
