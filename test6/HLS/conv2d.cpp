cfa11e8ffd6af29184045cd0f2a5b8c1dec5ed17
Eliot Abramo <eliot.abramo@epfl.ch>
2025-03-25 10:27:31 +0100

    test6 added

---
+++ test6/HLS/conv2d.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a524a162f16550560370ca102386348670ab9275aad173e1dc7f4211a59806b3
+size 6300
---
+++ test6/SW/model.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6792f1ac64f3436811047c658c5cfd6ce8ef04853d88c0270be45c682ae6f5b7
+size 18183
