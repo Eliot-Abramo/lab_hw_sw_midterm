7543bc2bed63a05f195b475eeab0f5a61ac7e26a
Eliot Abramo <eliot.abramo@epfl.ch>
2025-04-01 10:47:11 +0200

    working test 7

--- test7/HLS/conv2d.cpp
+++ test7/HLS/conv2d.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8cdad1070fe133912048144da3d97ab6c7dfad50f2de6555096d23094ad9dc1
-size 9777
+oid sha256:6855819626db40022f0d247d16ed6aae82e09728d870b8cd1ad59e50d3699b4f
+size 24669
