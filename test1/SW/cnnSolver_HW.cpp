5c1d1c858947a69ebeb528b753d163dec9487e60
Eliot Abramo <eliot.abramo@epfl.ch>
2025-04-03 01:34:06 +0200

    t1 SW

--- test1/SW/cnnSolver_HW.cpp
+++ test1/SW/cnnSolver_HW.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7222bac909e21f2a7c7d76d20aec7a4f84650bdec004a3898d61a0465957b9c9
-size 5879
+oid sha256:70d334105b7fe155bfaebe3dcce0589cd21ffd2e5e0ebaf5f766858af609986b
+size 5901
