046af8e6fe3edd9462bc75fefbbe06916e2fe6de
Eliot Abramo <eliot.abramo@epfl.ch>
2025-04-03 02:06:34 +0200

    added b and m

--- test1/SW/model.cpp
+++ test1/SW/model.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a533db3fa743e4c7c50c6fdeef1642ca9c4a7c64918bddbd3c584a78894d35ea
-size 18094
+oid sha256:9cc7e1acf2738b70d4a121776bd1404bf21396b0914dde24858e86527bd73371
+size 18739
