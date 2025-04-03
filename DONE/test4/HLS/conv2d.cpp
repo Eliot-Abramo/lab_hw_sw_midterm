779bc119e602fe187812703d8d481f7b77e076f1
Eliot Abramo <eliot.abramo@epfl.ch>
2025-04-03 21:57:07 +0200

    updated done

---
+++ DONE/test4/HLS/conv2d.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e53f8424051d1449398be80f0f7cd29806bf28fcd60e29a106aa17692bf27e3f
+size 4365
---
+++ DONE/test4/SW/CConv2DDriver.hpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c09bc3e3d086b73fdec60736f7d3b9f82dc94cf91234edcdb1b56ec29ecb9fd
+size 1506
